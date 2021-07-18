4e77dbf126be21a27e1511ecd7f45aa7b16d0c73
willson0v0 <willsonlgz@gmail.com>
2021-07-18 15:47:00 +0800

    clean up +

--- src/version.rs
+++
@@ -1,3 +0,0 @@
-//! This is a uname constant, and will be update automatically on building.
-/// NOTE: following line will be found and modified by build.rs. ***DONT CHANGE THIS LINE MANUALLY!!!!***
-pub const VERSION : &[u8] = b"Fri, 09 Jul 2021 09:40:30 +0000\0";
