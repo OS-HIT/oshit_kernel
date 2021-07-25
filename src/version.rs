f022ebffdd934f6e2a6bb1b3470c738fb75c1eae
willson0v0 <willsonlgz@gmail.com>
2021-07-25 16:41:45 +0800

    update git ignore

--- src/version.rs
+++
@@ -1,3 +0,0 @@
-//! This is a uname constant, and will be update automatically on building.
-/// NOTE: following line will be found and modified by build.rs. ***DONT CHANGE THIS LINE MANUALLY!!!!***
-pub const VERSION : &[u8] = b"Mon, 31 May 2021 11:44:08 +0000\0";
