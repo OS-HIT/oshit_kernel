f824402815e790d912a26e8ac8c27c614ee6a429
= <=>
2021-08-07 17:17:32 +0800

    add print_kernel_stack in utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -10,3 +10,29 @@ pub use mem_op::{
     strcpy,
     strlen
 };
+
+
+
+use crate::config::PAGE_SIZE;
+use crate::config::KERNEL_STACK_SIZE;
+use crate::config::TRAMPOLINE;
+#[allow(unused)]
+pub fn print_kernel_stack() {
+    if let Some(cp) = crate::process::current_process() {
+        let pid = cp.pid.0 as usize;
+        let top = TRAMPOLINE - pid * (KERNEL_STACK_SIZE + PAGE_SIZE);
+        let bottom = top - KERNEL_STACK_SIZE;
+        let mut sp:usize = 0;
+        unsafe {
+            llvm_asm!(
+                "mv $0, sp"
+                : "=r"(sp)
+                :::"volatile" 
+            );
+        }
+        let mask = 0x7fffffffff;
+        debug!("kstack of {}: {:#10X}-{:#10X}-{:#10X} used:{} left:{}", pid, bottom&mask, sp&mask, top&mask, top - sp, sp - bottom);
+    } else {
+        debug!("print_kernel_stack: No user process");
+    }
+}
